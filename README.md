91f8769c33ea4b151a79537fb232d99cb6bbeeef
Bertrand Benoit <contact@bertrand-benoit.net>
2019-02-18 13:33:55 +0100

    Implemented v1.1 of this tool. Added README.md documentation.

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
-# cloneToCleanGitRepositories
-Clone from a dirty Catchall Git repository to create clean repository for each specified versioned element
+# cloneToCleanGitRepositories version 1.1
+This is a free tool allowing to clone from a dirty Catchall Git repository to create clean repository for each specified versioned element.
+
+I created initially this tool after having posted a [question on Stackoverflow](https://stackoverflow.com/q/53396677/10524205).
+
+
+# Usage
+```
+Usage: ./cloneToCleanGitRepositories.sh <source repository> <dest root directory> <filter>
+<source repository>    path to the existing catchall git repository
+<dest root directory>  path to the existing root directory, in which git repositories will be created
+<filter>		       the (find) file pattern for which a git repository must be created
+
+N.B.: the source repository won't be altered in any way
+
+Sample:
+./cloneToCleanGitRepositories.sh /path/to/my/catchall/git/repository /tmp/myFirstTest '*.sh'
+```
+
+
+# Situation sample
+Imagine you have a single Git repository with several scripts in it, and now you would like a dedicated Git repository for each of your script; then this tool is perfect for you.
+
+```
+./cloneToCleanGitRepositories.sh /path/to/my/catchall/git/repository /tmp/myFirstTest '*.sh'
+```
+
+
+Information
+====
+Don't hesitate to contribute or to contact me if you want to improve the project.
+
+Bertrand BENOIT   <contact@bertrand-benoit.net>
---
+++ cloneToCleanGitRepositories.sh
@@ -0,0 +1,72 @@
+#!/bin/bash
+#
+# Author: Bertrand Benoit <mailto:contact@bertrand-benoit.net>
+# Version: 1.1
+#
+# Description: Create clean git repositories for EACH file matching specified filter, from specified source git repository (aka the first catchall one).
+# It is very interesting to create proper git repository, with a dedicated history, for your files which are currently all in the same Git repository.
+#
+# This script uses [scripts-common](https://github.com/bertrand-benoit/scripts-common)
+
+currentDir=$( dirname "$( which "$0" )" )
+. "$currentDir/scripts-common/utilities.sh"
+
+# usage: usage <name>
+function usage() {
+  echo -e "Usage: $0 <source repository> <dest root directory> <filter>"
+  echo -e "<source repository>\t\tpath to the existing catchall git repository"
+  echo -e "<dest root directory>\t\tpath to the existing root directory, in which git repositories will be created"
+  echo -e "<filter>\t\t\tthe (find) file pattern for which a git repository must be created"
+  echo -e "\nN.B.: the source repository won't be altered in any way"
+  echo -e "\nSample:\n$0 /path/to/my/catchall/git/repository /tmp/myFirstTest '*.sh'"
+
+  exit $ERROR_USAGE
+}
+
+# CLI light management.
+[ $# -lt 3 ] && usage
+SOURCE_REPO="$1"
+[ ! -d "$SOURCE_REPO" ] && errorMessage "Specified source Git repository '$SOURCE_REPO' does not exist."
+DEST_ROOT_DIR="$2"
+[ ! -d "$DEST_ROOT_DIR" ] && errorMessage "Specified destination root directory '$DEST_ROOT_DIR' does not exist."
+FILTER="$3"
+[ -z "$FILTER" ] && errorMessage "Specified filter '$FILTER' is NOT valide."
+
+sourceRepoName=$( basename "$SOURCE_REPO" )
+
+# For each file in root of the source git repository.
+function cloneToCleanGitRepositories() {
+  while IFS= read -r -d '' refToManage; do
+    # Safe-guard: refuse .git element path.
+    [[ "$refToManage" =~ /.*[.]git.*/ ]] && writeMessage "Ignoring $refToManage because it is part of Git repository metadata." && continue
+
+    writeMessage "Starting management of $refToManage ..."
+
+    refFileName=$( basename "$refToManage" )
+    newDestRepo="$DEST_ROOT_DIR/$refFileName"
+
+    # Creates the repository if not existing.
+    logFile="$newDestRepo/logFile.txt"
+    writeMessage "Creating new repository: '$newDestRepo', Log file: $logFile ... "
+
+    if [ ! -d "$newDestRepo" ]; then
+      mkdir -p "$newDestRepo"
+      cd "$newDestRepo"
+      ! git clone -q "$SOURCE_REPO" && errorMessage "Error while cloning source repository to $newDestRepo."
+    fi
+    cd "$newDestRepo/$sourceRepoName"
+
+    # Removes all other resources.
+    FILTER='git ls-tree -r --name-only --full-tree "$GIT_COMMIT" |grep -v ".git" |grep -v "'$refFileName'" |tr "\n" "\0" |xargs --no-run-if-empty -0 git rm -f --cached -r --ignore-unmatch'
+    ! git filter-branch -f --prune-empty --tag-name-filter cat --index-filter "$FILTER" -- --all >"$logFile" 2>&1 && errorMessage "Error while cleaning new git repository."
+
+    # Cleans remote information, if any, to ensure there is no push to the source repository.
+    [[ $( wc -l < <(git remote ) ) -gt 0 ]] && ! git remote remove origin >>"$logFile" 2>&1 && errorMessage "Error while removing remote."
+
+    writeMessage "Successfully completed management of $refToManage."
+  done < <(find "$SOURCE_REPO" -type f -name "$FILTER" -print0)
+}
+
+writeMessage "System will create a git repository for each file matching (find filter) '$FILTER', in a sub-directory of '$DEST_ROOT_DIR', from a clone of '$SOURCE_REPO'."
+cloneToCleanGitRepositories
+writeMessage "System completed all creation (if none has been created, check your filter)."
